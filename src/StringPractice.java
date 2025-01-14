1d4c7325d1fc5b75b1149bc491cbeb942ea48fc7
Alex_Borovets <Borovale002@gmail.com>
2025-01-13 22:36:05 -0800

    String Practice finished and added things I need to refresh on

--- src/StringPractice.java
+++ src/StringPractice.java
@@ -1,26 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringPractice {
   public static void main(String[] args) {
     // Create a string with at least 5 characters and assign it to a variable
-
+    String myString = "Hello";
     // Find the length of the string
-
+    int length = myString.length();
+    System.out.println("Length of the string: " + length);
     // Concatenate (add) two strings together and reassign the result
-
+    String newString = myString + " World";
+    System.out.println("Concatenated string: " + newString);
     // Find the value of the character at index 3
-
+    char charAtIndex3 = myString.charAt(3);
+    System.out.println("Character at index 3: " + charAtIndex3);
     // Check whether the string contains a given substring (i.e. does the string have "abc" in it?)
-
+    boolean containsSubstring = myString.contains("abc");
+    System.out.println("Does the string contain 'abc'? " + containsSubstring);
     // Iterate over the characters of the string, printing each one on a separate line
-
+    System.out.println("Characters in the string:");
+    for (int i = 0; i < myString.length(); i++) {
+      System.out.println(myString.charAt(i));
+    }
     // Create an ArrayList of Strings and assign it to a variable
-
+    List<String> stringList = new ArrayList<>();
     // Add multiple strings to the List (OK to do one-by-one)
-
+    stringList.add("Apples");
+    stringList.add("Carrots");
+    stringList.add("Pomegranites");
     // Join all of the strings in the list together into a single string separated by commas
     // Use a built-in method to achieve this instead of using a loop
-
+    String joinedString = String.join(", ", stringList);
+    System.out.println("Joined list into a single string: " + joinedString);
     // Check whether two strings are equal
-
+    String anotherString = "Hello";
+    boolean areStringsEqual = myString.equals(anotherString);
+    System.out.println("Are the two strings equal? " + areStringsEqual);
     /*
      * Reminder!
      * 
--- toRefresh.md
+++ toRefresh.md
@@ -2,4 +2,8 @@
 
 As you work through this exercise, write down anything that you needed to look up or struggled to remember here. It can be just a word or two (e.g. "joining strings"). You can use this as a guide of what to make extra sure you're refreshed on before exams and interviews.
 
-- 
+- String Practice
+- Set Practice
+- List Practice
+- Overall, I do need to look over all of these a little more to get them down
+- Because I am a little shaky on them.
